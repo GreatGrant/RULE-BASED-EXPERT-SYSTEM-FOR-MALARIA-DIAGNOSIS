55379321508e92f5ec126b6d7bb800a0a36d9b87
Grant Williams <greatgrant116@gmail.com>
2024-04-29 18:46:54 +0100

    create admin and attendant screens

---
+++ lib/screens/attendant_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AttendantScreen extends StatefulWidget {
+  const AttendantScreen({super.key});
+
+  @override
+  State<AttendantScreen> createState() => _AttendantScreenState();
+}
+
+class _AttendantScreenState extends State<AttendantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/main.dart
@@ -0,0 +1,269 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+import '../navigation/go_router.dart';
+import '../services/firebase_auth_methods.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MalariaApp());
+}
+
+class MalariaApp extends StatelessWidget {
+  const MalariaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+        title: const Text('Welcome', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.all(30),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              TabBar(
+                controller: _tabController,
+                tabs: const [
+                  Tab(text: 'Login'),
+                  Tab(text: 'Sign Up'),
+                ],
+                labelColor: Colors.blueGrey[900],
+                indicatorColor: Colors.blueGrey[900],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    LoginForm(),
+                    SignupForm(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text("Sign in to your account.", style: TextStyle(fontSize: 16),),
+          const SizedBox(height: 8),
+          const TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your email',
+              labelText: 'Email',
+              prefixIcon: Icon(Icons.email),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Enter your password',
+              labelText: 'Password',
+              prefixIcon: Icon(Icons.lock),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Todo() Add login validation logic here
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              backgroundColor: Colors.blueGrey[900],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            child: const Text('Login', style: TextStyle(color:  Colors.white),),
+          ),
+          const SizedBox(height: 8,),
+          TextButton(
+            onPressed: () {  },
+            child: Text("Forgot Password?",
+                style: TextStyle(
+                    color: Colors.blueGrey[900],
+                    fontWeight: FontWeight.bold
+                ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  SignupFormState createState() => SignupFormState();
+}
+
+class SignupFormState extends State
+{
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String? _validateEmail(String? email) {
+    if (email == null || email.isEmpty) {
+      return 'Please enter your email';
+    }
+    final RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!emailRegExp.hasMatch(email)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  void signUpUser(String firstName, String lastName, String email, String password, BuildContext context) {
+    FirebaseMethods(
+        FirebaseAuth.instance).signUpWithEmailAndPassword(
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        context: context,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(
+                  labelText: 'First Name',
+                  prefixIcon: Icon(Icons.book),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your first name' : null,
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Last Name',
+                  prefixIcon: Icon(Icons.book),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your last name' : null,
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+                validator: _validateEmail
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your password' : null,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Form is valid
+                    String firstName = _firstNameController.text;
+                    String lastName = _lastNameController.text;
+                    String email = _emailController.text;
+                    String password = _passwordController.text;
+                    signUpUser(firstName, lastName, email, password, context);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  backgroundColor: Colors.blueGrey[900],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: const Text('Sign Up', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
