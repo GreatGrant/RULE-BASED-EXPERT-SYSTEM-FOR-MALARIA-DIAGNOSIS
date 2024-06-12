1de2014257eb9256c9c0679c22a7f0ba2ea8f20b
Grant Williams <greatgrant116@gmail.com>
2024-06-12 10:59:15 +0100

    redesign diagnosis page

--- lib/screens/diagnosis/diagnosis_screen.dart
+++ lib/screens/diagnosis/diagnosis_screen.dart
@@ -12,27 +12,13 @@ class DiagnosisScreen extends StatefulWidget {
 }
 
 class DiagnosisScreenState extends State<DiagnosisScreen> {
-  String diagnosisResult = 'Unknown'; // Initialize diagnosis result
+  String diagnosisResult = 'Unknown';
   double diagnosisProbability = 0.0;
   List<String> selectedSymptoms = [];
   List<String> allSymptoms = [
-    'Fever',
-    'Chills',
-    'Headache',
-    'Nausea',
-    'Cough',
-    'Sore throat',
-    'Muscle Aches',
-    'Fatigue',
-    'Vomiting',
-    'Diarrhea',
-    'Sweating',
-    'Weakness',
-    'Loss of Appetite',
-    'Dizziness',
-    'Abdominal Pain',
-    'Jaundice',
-    'Rapid Heart Rate',
+    'Fever', 'Chills', 'Headache', 'Nausea', 'Cough', 'Sore throat', 'Muscle Aches',
+    'Fatigue', 'Vomiting', 'Diarrhea', 'Sweating', 'Weakness', 'Loss of Appetite',
+    'Dizziness', 'Abdominal Pain', 'Jaundice', 'Rapid Heart Rate',
   ];
 
   void _toggleSymptom(String symptom) {
@@ -46,23 +32,21 @@ class DiagnosisScreenState extends State<DiagnosisScreen> {
   }
 
   void _startDiagnosis() {
-    // Call diagnosis function with selected symptoms
     DiagnosisResult result = diagnoseMalaria(selectedSymptoms);
-    // Assign diagnosis result and probability
-    diagnosisResult = result.diagnosis;
-    diagnosisProbability = result.probability;
+    setState(() {
+      diagnosisResult = result.diagnosis;
+      diagnosisProbability = result.probability;
+    });
 
-    // Define recommendations based on diagnosis
     String recommendation = '';
     if (diagnosisResult == 'Malaria') {
-      recommendation = 'Please advise the patient to consult a healthcare professional immediately for further evaluation and treatment.';
+      recommendation = 'Please consult a healthcare professional immediately for further evaluation and treatment.';
     } else if (diagnosisResult == 'Possible Malaria') {
-      recommendation = 'Please consider further assessment by a healthcare professional.';
+      recommendation = 'Consider further assessment by a healthcare professional.';
     } else {
-      recommendation = "It's advisable to monitor the patient's symptoms closely and recommend seeking medical advice if they persist or worsen.";
+      recommendation = 'Monitor the symptoms closely and seek medical advice if they persist or worsen.';
     }
 
-    // Show diagnosis result after diagnosis
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -74,7 +58,7 @@ class DiagnosisScreenState extends State<DiagnosisScreen> {
               children: [
                 Text('Diagnosis: $diagnosisResult'),
                 Text('Probability: ${(diagnosisProbability * 100).toStringAsFixed(2)}%'),
-                Text('Recommendation: $recommendation'), // Add recommendation here
+                Text('Recommendation: $recommendation'),
                 SizedBox(
                   height: 250,
                   child: MalariaDiagnosisChart(probability: diagnosisProbability),
@@ -107,151 +91,77 @@ class DiagnosisScreenState extends State<DiagnosisScreen> {
 
   void _saveDiagnosisResult(double diagnosisProbability) {
     PatientHelper.saveDiagnosis(
-        diagnosisResult: diagnosisResult,
-        diagnosisProbability: diagnosisProbability,
-        patientId: widget.patientId,
-        context: context
+      diagnosisResult: diagnosisResult,
+      diagnosisProbability: diagnosisProbability,
+      patientId: widget.patientId,
+      context: context,
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
-        title: Text('Malaria Diagnosis'),
-        backgroundColor: Colors.blueGrey[100],
+        title: const Text('Malaria Diagnosis'),
         actions: [
           IconButton(
             onPressed: () {
               UserHelper.logOut();
             },
-            icon: Icon(Icons.logout, color: Colors.blueGrey[700],),
+            icon: const Icon(Icons.logout, color: Colors.white),
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
               'Select Symptoms',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+              style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: allSymptoms.length,
+                itemBuilder: (context, index) {
+                  final symptom = allSymptoms[index];
+                  final isSelected = selectedSymptoms.contains(symptom);
+                  return ElevatedButton(
+                    onPressed: () => _toggleSymptom(symptom),
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        isSelected ? Colors.blueGrey[900] : Colors.grey,
+                      ),
+                    ),
+                    child: Text(symptom, style: const TextStyle(color: Colors.white)),
+                  );
+                },
               ),
-              textAlign: TextAlign.center,
             ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.grey[300],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: Text(
-                            'All Symptoms',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Expanded(
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: allSymptoms.map((symptom) {
-                                bool isSelected = selectedSymptoms.contains(symptom);
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      _toggleSymptom(symptom);
-                                    },
-                                    style: ButtonStyle(
-                                      backgroundColor: MaterialStateProperty.all(isSelected ? Colors.blueGrey[900] : Colors.grey),
-                                    ),
-                                    child: Text(symptom, style: const TextStyle(color: Colors.white)),
-                                  ),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.grey[300],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: Text(
-                            'Selected Symptoms',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Expanded(
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: selectedSymptoms.map((symptom) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      _toggleSymptom(symptom);
-                                    },
-                                    style: ButtonStyle(
-                                      backgroundColor: MaterialStateProperty.all(Colors.blueGrey[900]),
-                                    ),
-                                    child: Text(symptom, style: const TextStyle(color: Colors.white)),
-                                  ),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            const SizedBox(height: 16.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: selectedSymptoms.isEmpty
+                    ? () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please select at least one symptom.')),
+                  );
+                }
+                    : _startDiagnosis,
+                child: const Text('Diagnose', style: TextStyle(color: Colors.white, fontSize: 16)),
+              ),
             ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blueGrey[900])),
-            onPressed: () {
-              if (selectedSymptoms.isEmpty) {
-                // Show error message if no symptoms are selected
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Please select at least one symptom.'),
-                  ),
-                );
-              } else {
-                _startDiagnosis();
-              }
-            },
-            child: const Text('Diagnose', style: TextStyle(color: Colors.white),),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -260,30 +170,20 @@ class DiagnosisScreenState extends State<DiagnosisScreen> {
 class DiagnosisResult {
   final String diagnosis;
   final double probability;
-  final List<String> symptoms;
 
-  DiagnosisResult(this.diagnosis, this.probability, this.symptoms);
+  DiagnosisResult(this.diagnosis, this.probability);
 }
 
 DiagnosisResult diagnoseMalaria(List<String> symptoms) {
-  // Rule-based logic for diagnosing malaria
-  // Simulating complex rules for demonstration purposes
-
-  // Initialize diagnosis and probability
   String diagnosis = 'Unknown';
   double probability = 0.0;
-  List<String> matchedSymptoms = [];
 
-  // Define all symptom combinations and their corresponding diagnoses and probabilities
   Map<List<String>, Map<String, double>> rules = {
-    // Malaria
     ['Fever', 'Chills', 'Headache', 'Nausea', 'Muscle Aches', 'Fatigue', 'Vomiting']: {'Malaria': 0.9},
     ['Fever', 'Chills', 'Headache', 'Nausea', 'Muscle Aches', 'Fatigue']: {'Possible Malaria': 0.85},
     ['Fever', 'Chills', 'Headache', 'Nausea', 'Muscle Aches']: {'Possible Malaria': 0.7},
     ['Fever', 'Chills', 'Headache', 'Nausea']: {'Possible Malaria': 0.65},
     ['Fever', 'Chills', 'Headache']: {'Possible Malaria': 0.6},
-
-    // Other Diagnoses
     ['Headache']: {'Migraine': 0.8},
     ['Fever', 'Headache']: {'Possible Cold': 0.5},
     ['Fever', 'Muscle Aches', 'Fatigue']: {'Flu': 0.7},
@@ -301,8 +201,6 @@ DiagnosisResult diagnoseMalaria(List<String> symptoms) {
     ['Fever', 'Diarrhea']: {'Possible Food Poisoning': 0.5},
     ['Diarrhea']: {'Gastrointestinal Infection': 0.6},
     ['Muscle Aches']: {'Muscle Strain': 0.6},
-
-    // Additional Rules
     ['Fever', 'Chills', 'Headache', 'Fatigue', 'Sweating']: {'Possible Malaria': 0.75},
     ['Fever', 'Chills', 'Headache', 'Muscle Aches', 'Sweating']: {'Possible Malaria': 0.8},
     ['Fever', 'Chills', 'Headache', 'Fatigue', 'Loss of Appetite']: {'Possible Malaria': 0.7},
@@ -313,21 +211,18 @@ DiagnosisResult diagnoseMalaria(List<String> symptoms) {
     ['Fever', 'Chills', 'Headache', 'Nausea', 'Weakness']: {'Possible Malaria': 0.7},
   };
 
-  // Check if any symptom combination matches the selected symptoms
   for (var entry in rules.entries) {
     List<String> symptomsCombination = entry.key;
     Map<String, double> diagnosisInfo = entry.value;
 
     if (symptoms.every((symptom) => symptomsCombination.contains(symptom))) {
-      // Calculate probability based on the number of matching symptoms
       probability = diagnosisInfo.values.first * (symptoms.length / symptomsCombination.length);
       diagnosis = diagnosisInfo.keys.first;
-      matchedSymptoms = symptomsCombination;
       break;
     }
   }
 
-  return DiagnosisResult(diagnosis, probability, matchedSymptoms);
+  return DiagnosisResult(diagnosis, probability);
 }
 
 class MalariaDiagnosisChart extends StatelessWidget {
@@ -356,8 +251,8 @@ class MalariaDiagnosisChart extends StatelessWidget {
 }
 
 class ChartSampleData {
-  final String? x;
+  final String x;
   final double y;
 
-  ChartSampleData({this.x, required this.y});
+  ChartSampleData({required this.x, required this.y});
 }
